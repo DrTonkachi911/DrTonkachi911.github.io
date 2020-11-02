9e368dde1dfba9eea6e204a12931bb411f73422e
DrTonkachi911 <zakkjpn@yahoo.co.jp>
2020-11-02 23:50:07 +0900

    updated  animation

---
+++ js/animation.js
@@ -0,0 +1,22 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".members", {
+    delay:500,
+    duration:3000
+});
+
+sr.reveal(".masthead", {
+    delay:300,
+    duration:3000
+});
+
+sr.reveal(".masthead .text-uppercase", {
+    delay:2000,
+    duration:3000
+});
+
+sr.reveal(".newstable", {
+    scale:0,
+    delay:300,
+    duration:2000
+});
