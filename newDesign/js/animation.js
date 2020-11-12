664a9a5b9e517a63c9c92df092bb9ad6b6754459
KiritoSword <zakkjpn@protonmail.com>
2020-11-12 14:57:43 +0900

    updated

---
+++ newDesign/js/animation.js
@@ -0,0 +1,57 @@
+window.sr = ScrollReveal();
+
+sr.reveal(".top-section", {
+    delay:300,
+    duration:2000
+});
+
+sr.reveal(".news-section h1", {
+    delay:300,
+    duration:1000
+});
+
+sr.reveal(".news-section .newstable", {
+    delay:300,
+    duration:1200,
+    scale:0,
+    opacity:1
+});
+
+sr.reveal(".about-section h1", {
+    delay:300,
+    duration:1200
+});
+
+sr.reveal(".about-section p", {
+    delay:300,
+    duration:1200
+});
+
+sr.reveal(".about-section img", {
+    delay:600,
+    duration:1200,
+    scale:0,
+    opacity:1
+});
+
+sr.reveal(".contact-section ul", {
+    delay:300,
+    duration:1000,
+    origin:"bottom",
+    distance:"50%"
+});
+
+sr.reveal(".member-section h1", {
+    delay:300,
+    duration:1000
+});
+
+sr.reveal(".member-section .tab-wrap", {
+    delay:300,
+    duration:1200,
+    opacity:0,
+    origin:"bottom",
+    distance:"30%"
+});
+
+sr.clean(".member-section .tab-wrap .tab-content h1");
---
+++ newDesign/js/load_news.js
@@ -0,0 +1,23 @@
+$.getJSON("./json/news.json", function(data) {
+    var content = document.getElementById("news-content");
+    var table_obj = document.createElement("table");
+    var length = data.length;
+    for (var i = length - 1; i >= 0; i--) {
+        var json = data[i];
+        
+        var tr_obj = document.createElement("tr");
+        var th_obj = document.createElement("th");
+        var text1 = document.createTextNode(json.date);
+        th_obj.appendChild(text1);
+        tr_obj.appendChild(th_obj);
+        var td_obj = document.createElement("td");
+        var a_obj = document.createElement("a");
+        a_obj.href = json.href;
+        var text2 = document.createTextNode(json.title);
+        a_obj.appendChild(text2);
+        td_obj.appendChild(a_obj);
+        tr_obj.appendChild(td_obj);
+        table_obj.appendChild(tr_obj);
+    }
+    content.appendChild(table_obj);
+});
