97e675e13a46998701b57c2acb187dcd075a3324
KiritoSword <zakkjpn@protonmail.com>
2020-11-24 18:25:05 +0900

    updated

--- js/animation.js
+++ js/animation.js
@@ -1,13 +1,8 @@
 window.sr = ScrollReveal();
 
-sr.reveal(".top-section", {
-    delay:300,
-    duration:2000
-});
-
 sr.reveal(".home-section", {
     delay:300,
-    duration:2000
+    duration:1000
 });
 
 sr.reveal(".news-section h1", {
@@ -48,15 +43,3 @@ sr.reveal(".contact-section ul", {
     delay:300,
     duration:1000
 });
-
-sr.reveal(".member-section h1", {
-    delay:300,
-    duration:1000
-});
-
-sr.reveal(".member-section .tab-wrap", {
-    delay:300,
-    duration:1200,
-});
-
-sr.clean(".member-section .tab-wrap .tab-content h1");
